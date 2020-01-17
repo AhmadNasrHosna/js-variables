13c785ebd1822595a70b78a0429c9b0f4b6285f1
Ahmed <ahmednhosna@gmail.com>
2020-01-16 23:27:08 -0800

    All is done

---
+++ App.js
@@ -0,0 +1,16 @@
+let admin;
+let name;
+name = "John";
+admin = name;
+
+let ourPlanetName = "Earth";
+
+let currentUserName = name;
+
+const birthday = '18.04.1982';
+const age = someCode(birthday);
+
+
+alert(admin);
+alert(currentUserName);
+alert(ourPlanetName);
